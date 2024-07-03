e8e0a86ef512d5052c747eac933291a903c547e4
Dar2904 <romanovdima576@gmail.com>
2024-07-03 20:19:32 +0300

    first commit

---
+++ AndroidBoard.mk
@@ -0,0 +1,12 @@
+# Hack for build
+$(shell mkdir -p $(OUT)/system/vendor/firmware)
+$(shell cp $(ANDROID_BUILD_TOP)/device/samsung/vivalto3gvn/prop/vbc_eq $(OUT)/system/vendor/firmware)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+
+# include the non-open-source counterpart to this file
+-include vendor/samsung/vivalto3gvn/AndroidBoardVendor.mk
